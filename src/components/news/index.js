b80b3a39b922d4165c5e74f99669689bfbbcde20
Tony-Stark <vaibhav27.neo@gmail.com>
2018-12-27 02:16:23 +0530

    removing logs

--- src/components/news/index.js
+++ src/components/news/index.js
@@ -1,18 +1,12 @@
 import React, { Component } from "react";
-import { withStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 
 class News extends Component {
-  //   state = {};
-
   render() {
-    // console.log(this.props);
     return (
       <div className="card">
         <Card className="">
