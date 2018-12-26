29687401dd6a1d764abbcf549272eba51f60d510
Tony-Stark <vaibhav27.neo@gmail.com>
2018-12-27 01:44:52 +0530

    upadting style

--- src/App.js
+++ src/App.js
@@ -1,27 +1,60 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import News from "./components/news/index";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://127.0.0.1:3000/api/news")
+      .then(res => res.json())
+      .then(
+        result => {
+          this.setState({
+            isLoaded: true,
+            items: result
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        error => {
+          console.log(error);
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      );
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div className="container">
+          <ul>
+            {items.map(item => (
+              <News data={item} />
+              // <li key={item.title}>
+              //   {item.content} {item.image}
+              // </li>
+            ))}
+          </ul>
+        </div>
+      );
+    }
   }
 }
 
---
+++ src/components/news/index.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+class News extends Component {
+  //   state = {};
+
+  render() {
+    // console.log(this.props);
+    return (
+      <div className="card">
+        <Card className="">
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              alt="Contemplative Reptile"
+              className=""
+              height="140"
+              image={this.props.data.image}
+              title="Contemplative Reptile"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                {this.props.data.title}
+              </Typography>
+              <Typography component="p">
+                {this.props.data.description}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </div>
+    );
+  }
+}
+
+export default News;
